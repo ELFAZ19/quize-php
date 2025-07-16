a11aa9a6e7691094178a7245289957fb78878813
ELFAZ19 <elfazdejene@gmail.com>
2025-07-16 17:47:04 +0300

    commit

--- results.php
+++ results.php
@@ -169,7 +169,6 @@ unset($_SESSION['end_time']);
                     <path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"></path>
                 </svg>
                 Retake Quiz 
-                
             </a>
         </div>
     </div>
